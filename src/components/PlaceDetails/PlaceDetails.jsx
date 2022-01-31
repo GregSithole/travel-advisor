ac3036cd1205ce3f3b7b1a29be6b2f4fca244c78
Greg Sithole <gregsithole@hotmail.co.za>
2022-01-31 15:46:34 +0200

    Updated PlaceDetails.jsx

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,8 +1,67 @@
 import React from 'react';
+import {Box, Typography, Button, Card, CardMedia, CardContent, CardActions, Chip} from "@material-ui/core";
+import {LocationOn, Phone} from "@material-ui/icons";
+import {Rating} from "@material-ui/lab";
 
-const PlaceDetails = (props) => {
+import useStyles from './styles';
+
+const PlaceDetails = ({place, selected, refProp}) => {
+    const classes = useStyles();
+
+    if (selected) {
+        refProp?.current?.scrollIntoView({behavior: "smooth", block: "start"});
+    }
+
     return (
-        <h1>{props.place.name}</h1>
+        <Card elevation={6}>
+            <CardMedia
+                style={{height: 200}}
+                image={place.photo ? place.photo.images.large.url : 'https://via.placeholder.com/200?text=No+Image'}
+                title={place.name}
+            />
+            <CardContent>
+                <Typography gutterBottom variant={"h5"}>{place.name}</Typography>
+                <Box display="flex" justifyContent="space-between">
+                    <Rating value={Number(place.rating)} readOnly/>
+                    <Typography gutterBottom variant="subtitle1">out of {place.num_reviews} reviews</Typography>
+                </Box>
+                <Box display="flex" justifyContent="space-between">
+                    <Typography variant="subtitle1">Price</Typography>
+                    <Typography gutterBottom variant="subtitle1">{place.price_level}</Typography>
+                </Box>
+                <Box display="flex" justifyContent="space-between">
+                    <Typography variant="subtitle1">Ranking</Typography>
+                    <Typography gutterBottom variant="subtitle1">{place.ranking}</Typography>
+                </Box>
+                {place?.awards?.map((award) => (
+                    <Box my={1} display="flex" justifyContent="space-between" alignItems="center">
+                        <img src={award.images.small} alt={award.display_name}/>
+                        <Typography variant="subtitle2" color="textSecondary">{award.display_name}</Typography>
+                    </Box>
+                ))}
+                {place?.cuisine?.map((cuisine) => (
+                    <Chip key={cuisine.name} size={"small"} label={cuisine.name} className={classes.chip}/>
+                ))}
+                {place?.address && (
+                    <Typography variant="subtitle2" color="textSecondary">
+                        <LocationOn/> {place.address}
+                    </Typography>
+                )}
+                {place?.phone && (
+                    <Typography variant="subtitle2" color="textSecondary">
+                        <Phone/> {place.phone}
+                    </Typography>
+                )}
+                <CardActions>
+                    <Button size="small" color="primary" onClick={() => window.open(place.web_url, '_blank')}>
+                        Trip Advisor
+                    </Button>
+                    <Button size="small" color="primary" onClick={() => window.open(place.website, '_blank')}>
+                        Website
+                    </Button>
+                </CardActions>
+            </CardContent>
+        </Card>
     )
 }
 
